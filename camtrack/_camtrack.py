69f7c22c4e14ca4032c26b12dc6835ab5b5a31bd
Bogdan Bugaev <bogdan.bugaev@gmail.com>
2020-02-23 16:28:33 +0300

    Add a minor fix to PointCloudBuilder

--- camtrack/_camtrack.py
+++ camtrack/_camtrack.py
@@ -258,7 +258,7 @@ class PointCloudBuilder:
     def __init__(self, ids: np.ndarray = None, points: np.ndarray = None,
                  colors: np.ndarray = None) -> None:
         super().__init__()
-        self._ids = ids if ids is not None else np.array([])
+        self._ids = ids if ids is not None else np.array([], dtype=np.int64)
         self._points = points if points is not None else np.array([])
         self._colors = colors
         self._sort_data()
