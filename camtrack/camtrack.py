f389088b96a72097b528bde0a430197806e4d994
vasyoid <vasyoid@gmail.com>
2020-03-10 01:26:36 +0300

    Implement camera initialization

--- camtrack/camtrack.py
+++ camtrack/camtrack.py
@@ -22,8 +22,49 @@ from _camtrack import (
     triangulate_correspondences,
     TriangulationParameters,
     rodrigues_and_translation_to_view_mat3x4,
-    pose_to_view_mat3x4
+    pose_to_view_mat3x4,
+    eye3x4,
+    remove_correspondences_with_ids
 )
+
+
+def init_camera_positions(corner_storage, intrinsic_mat, triang_params):
+    best_pose = None
+    best_cloud_size = -1
+    best_ind = -1
+    total_frames = len(corner_storage)
+    for i, frame in enumerate(corner_storage):
+        print(f'\rInitializing camera positions. [{i + 2} frames of {total_frames}], ', end='')
+        pose, cloud_size = pose_by_two_frames(corner_storage[0], frame, intrinsic_mat, triang_params)
+        if cloud_size > best_cloud_size:
+            best_pose = pose
+            best_cloud_size = cloud_size
+            best_ind = i
+        print(f'{cloud_size} points   ', end='')
+    print(f'\nframes 0 and {best_ind} have been chosen, {best_cloud_size} correspondences found')
+    return (0, eye3x4()), (best_ind, pose_to_view_mat3x4(best_pose))
+
+
+def pose_by_two_frames(frame1, frame2, intrinsic_mat, triang_params):
+    correspondences = build_correspondences(frame1, frame2)
+    if correspondences.ids.shape[0] < 7:
+        return None, 0
+    mat, mat_mask = cv2.findEssentialMat(correspondences.points_1, correspondences.points_2, intrinsic_mat,
+                                         method=cv2.RANSAC, prob=0.99, threshold=1)
+    mat_mask = mat_mask.flatten()
+    correspondences = remove_correspondences_with_ids(correspondences, np.argwhere(mat_mask == 0))
+    r1, r2, t = cv2.decomposeEssentialMat(mat)
+    best_pose = None
+    best_points = -1
+    for mat in (r1.T, r2.T):
+        for vec in (t, -t):
+            pose = Pose(mat, mat @ vec)
+            points, _, _ = triangulate_correspondences(correspondences, eye3x4(), pose_to_view_mat3x4(pose),
+                                                       intrinsic_mat, triang_params)
+            if points.shape[0] > best_points:
+                best_pose = pose
+                best_points = points.shape[0]
+    return best_pose, best_points
 
 
 def track_and_calc_colors(camera_parameters: CameraParameters,
@@ -35,9 +76,6 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
                           min_angle: float = 1,
                           min_depth: float = 0.1) \
         -> Tuple[List[Pose], PointCloud]:
-    if known_view_1 is None or known_view_2 is None:
-        raise NotImplementedError()
-
     triang_params = TriangulationParameters(max_reprojection_error=reproj_err,
                                             min_triangulation_angle_deg=min_angle,
                                             min_depth=min_depth)
@@ -47,14 +85,21 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
         camera_parameters,
         rgb_sequence[0].shape[0]
     )
-    mat_1 = pose_to_view_mat3x4(known_view_1[1])
-    mat_2 = pose_to_view_mat3x4(known_view_2[1])
+
+    if known_view_1 is None or known_view_2 is None:
+        known_view_1, known_view_2 = init_camera_positions(corner_storage, intrinsic_mat, triang_params)
+        mat_1 = known_view_1[1]
+        mat_2 = known_view_2[1]
+    else:
+        mat_1 = pose_to_view_mat3x4(known_view_1[1])
+        mat_2 = pose_to_view_mat3x4(known_view_2[1])
 
     points, ids = build_and_triangulate_correspondences(intrinsic_mat,
                                                         corner_storage[known_view_1[0]], mat_1,
                                                         corner_storage[known_view_2[0]], mat_2,
                                                         triang_params)
-    if len(points) < 10:
+    if len(points) < 7:
+        print(f'too few correspondences: {points.shape[0]}')
         exit(0)
 
     total_frames = len(corner_storage)
@@ -72,8 +117,11 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
         for cur_frame, corners in enumerate(corner_storage):
             if tracked_mats[cur_frame] is not None:
                 continue
-            updated = updated or try_update(corner_storage, corners, cur_frame, intrinsic_mat, cloud_points, total_frames,
+            updated = updated or try_update(corner_storage, corners, cur_frame, intrinsic_mat, cloud_points,
+                                            total_frames,
                                             tracked_mats, triang_params)
+
+    print()
 
     point_cloud_builder = PointCloudBuilder(
         ids=np.array([i for i, point in enumerate(cloud_points) if point is not None]),
@@ -109,7 +157,8 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
     return cloud_points, point_cloud
 
 
-def try_update(corner_storage, corners, cur_frame, intrinsic_mat, cloud_points, total_frames, tracked_mats, triang_params):
+def try_update(corner_storage, corners, cur_frame, intrinsic_mat, cloud_points, total_frames, tracked_mats,
+               triang_params):
     ids = corners.ids.flatten()
     mask = np.array([cloud_points[id] is not None for id in ids])
     try:
@@ -118,12 +167,12 @@ def try_update(corner_storage, corners, cur_frame, intrinsic_mat, cloud_points, 
         _, rvec, tvec, inliers = cv2.solvePnPRansac(np.array([cloud_points[ind] for ind in ids[mask]]),
                                                     corners.points[mask], intrinsic_mat, None)
         inliers = inliers.flatten()
-        print(f'Tracking corners. [{cur_frame} frames of {total_frames}] {len(inliers)} inliers')
+        print(f'\rTracking corners. [{cur_frame + 1} frames of {total_frames}] {len(inliers)} inliers', end='')
         for id in ids:
             cloud_points[id] = None if id not in inliers else cloud_points[id]
         tracked_mats[cur_frame] = rodrigues_and_translation_to_view_mat3x4(rvec, tvec)
     except:
-        print(f'Tracking corners. [{cur_frame} frames of {total_frames}] no inliers')
+        print(f'\rTracking corners. [{cur_frame + 1} frames of {total_frames}] no inliers', end='')
         return False
     if tracked_mats[cur_frame] is not None:
         for another_frame in range(total_frames):
